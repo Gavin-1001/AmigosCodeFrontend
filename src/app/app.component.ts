863eb64648628467475c21301ff7299cea5d1685
Gavin Shelley <gavinshelley10@gmail.com>
2023-01-17 15:29:20 +0000

    Added to component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from "./common/employee";
+import {EmployeeService} from "./service/employee.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+    //title = 'frontend';
+
+    employees: Employee[];
+
+    constructor(private employeeService: EmployeeService) {
+    }
+
+    ngOnInit() {
+        this.getEmployees();
+    }
+
+
+    public getEmployees(): void {
+        this.employeeService.getEmployees()
+            .subscribe(
+                (response:Employee[]) => {
+                    this.employees = response
+                },
+                (error: HttpErrorResponse) => {
+                        alert(error.message);
+                }
+            );
+    }
+
+
+
 }
--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,32 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Employee} from "../common/employee";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class EmployeeService {
 
-  private baseUrl = '';
+    private baseUrl = 'http://localhost:8080';
 
-  constructor(private httpClient: HttpClient) {
-  }
+    constructor(private httpClient: HttpClient) {
+    }
 
-  public getEmployees(): Observable<Employee[]>{
-    return this.httpClient.get<Employee[]>(`${this.baseUrl}/employee/getAll`);
-  }
+    public getEmployees(): Observable<Employee[]> {
+        return this.httpClient.get<Employee[]>(`${this.baseUrl}/employee/getAll`);
+    }
 
-  public addEmployee(employee: Employee): Observable<Employee>{
-    return this.httpClient.post<Employee>(`${this.baseUrl}/employee/add`, employee);
-  }
+    public addEmployee(employee: Employee): Observable<Employee> {
+        return this.httpClient.post<Employee>(`${this.baseUrl}/employee/add`, employee);
+    }
 
-  public updateEmployee(employee:Employee): Observable<Employee>{
-    return this.httpClient.put<Employee>(`${this.baseUrl}/employee/update`, employee);
-  }
+    public updateEmployee(employee: Employee): Observable<Employee> {
+        return this.httpClient.put<Employee>(`${this.baseUrl}/employee/update`, employee);
+    }
 
-  public deleteEmployee(employeeId:number):Observable<void>{
-    return this.httpClient.delete<void>(`${this.baseUrl}/employee/delete/${employeeId}`);
-  }
+    public deleteEmployee(employeeId: number): Observable<void> {
+        return this.httpClient.delete<void>(`${this.baseUrl}/employee/delete/${employeeId}`);
+    }
 
 }
